283ed49288104616d492995ec4d6d78db714bf28
epuir <130913500+epuir@users.noreply.github.com>
2025-03-17 23:22:24 +0800

    try

---
+++ Assets/Scripts/Tools/Excel/CSV.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Tools.Excel
+{
+    public static class CSV
+    {
+        public static int AllLine;
+
+        public static void LoadFile(string filepath, Action<List<string[]>> a)
+        {
+            if (!File.Exists(filepath))
+            {
+                Debug.LogError(filepath + " no found");
+                return;
+            }
+
+            StreamReader sr = null;
+            try
+            {
+                Debug.LogError("开始");
+                sr = File.OpenText(filepath);
+                List<string[]> content = new List<string[]>();
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    content.Add(line.Split(',')); //csv格式
+                }
+
+                sr.Close();
+                sr.Dispose();
+                Debug.Log("Line：" + content.Count);
+                AllLine = content.Count;
+                a?.Invoke(content);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tools/Excel/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Tools.Excel
+{
+    public class Test:MonoBehaviour
+    {
+        private void Start()
+        {
+            var t=  Application.streamingAssetsPath;
+            var d =  Directory.GetParent(Application.dataPath);
+            string path = Path.Combine(t, "Text.csv");
+            CSV.LoadFile( "D:\\game creat\\CatGame\\Excel\\Test.csv\\", ( s) =>
+            {
+                foreach (var VARIABLE in s)
+                {
+                    Debug.LogError("/n");
+                    foreach (var VA in VARIABLE)
+                    {
+                        Debug.Log(VA);
+                    }
+                }
+            });
+            
+        }
+    }
+}
